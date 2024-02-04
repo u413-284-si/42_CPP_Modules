83481979aa02d72c0b5bc087d91a3b632312a384
sirui <sirui.qiu01@gmail.com>
2024-02-04 17:02:04 +0100

    feat[cpp00, ex01]: add Contact class header

---
+++ cpp00/ex01/inc/Contact.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/03 08:58:55 by sqiu              #+#    #+#             */
+/*   Updated: 2024/02/04 16:51:10 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+/* ====== LIBRARIES ====== */
+
+#include <iostream>
+#include <string>
+
+/* ====== CLASSES ====== */
+
+class Contact{
+	public:
+				Contact(void);
+				~Contact(void);
+
+				std::string	getFirstName(void) const;
+				std::string getLastName(void) const;
+				std::string getNickname(void) const;
+				std::string getPhoneNumber(void) const;
+				std::string getDarkestSecret(void) const;
+				void		getContactData(void) const;
+				void		setFirstName(std::string firstName);
+				void		setLastName(std::string lastName);
+				void		setNickname(std::string nickname);
+				void		setPhoneNumber(std::string phoneNumber);
+				void		setDarkestSecret(std::string darkestSecret);
+				void		setContactData(std::string firstName, \
+										   std::string lastName, \
+										   std::string nickname, \
+										   std::string phoneNumber, \
+										   std::string darkestSecret);
+			
+	private:
+				std::string	mFirstName;
+				std::string	mLastName;
+				std::string mNickname;
+				std::string	mPhoneNumber;
+				std::string mDarkestSecret;
+};
+
+#endif
