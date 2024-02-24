7632e0c0f25520a28ad1cf4b705f67ae8f3fcbab
Sirui <sirui.qiu01@gmail.com>
2024-02-24 14:04:45 +0100

    feat[cpp01, ex05]: add functions to class

--- cpp01/ex05/src/Harl.cpp
+++ cpp01/ex05/src/Harl.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Harl.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*   By: u413q <u413q@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 12:25:18 by sqiu              #+#    #+#             */
-/*   Updated: 2024/02/24 12:30:16 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/02/24 14:04:12 by u413q            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,25 @@ Harl::~Harl(void){
 	return;
 }
 
-void	complain(std::string level){
+void	Harl::complain(std::string level){
 	
 }
 
-void	debug(void){
+void	Harl::debug(void){
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 	return;
 }
 
-void	info(void){
+void	Harl::info(void){
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
-void	warning(void){
+void	Harl::warning(void){
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 	return;
 }
 
-void	error(void){
+void	Harl::error(void){
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 	return;
 }
