aca6fc7df0025ad10d51325a266a3060ee7294d4
Sirui <sirui.qiu01@gmail.com>
2024-02-24 14:51:04 +0100

    refactor[cpp01, ex05]:  format messages

--- cpp01/ex05/src/Harl.cpp
+++ cpp01/ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 12:25:18 by sqiu              #+#    #+#             */
-/*   Updated: 2024/02/24 14:41:31 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/02/24 14:50:23 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,20 +42,24 @@ int		Harl::convertLevel(const std::string level){
 }
 
 void	Harl::debug(void){
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << "[ DEBUG ]\n";
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI really do!" << std::endl;
 	return;
 }
 
 void	Harl::info(void){
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "[ INFO ]\n";
+	std::cout << "I cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger!\nIf you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning(void){
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]\n";
+	std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month." << std::endl;
 	return;
 }
 
 void	Harl::error(void){
+	std::cout << "[ ERROR ]\n";
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 	return;
 }
