b3612fb367bf207926ad274bb2e07571298470cd
Sirui <sirui.qiu01@gmail.com>
2024-02-24 14:13:49 +0100

    feat[cpp01, ex05]: add header declaration for pointer to member function

--- cpp01/ex05/inc/Harl.hpp
+++ cpp01/ex05/inc/Harl.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*   By: u413q <u413q@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 12:07:44 by sqiu              #+#    #+#             */
-/*   Updated: 2024/02/24 12:24:36 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/02/24 14:13:12 by u413q            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@
 
 #include <iostream>
 
+/* ====== TYPEDEFS ====== */
+
+typedef void	(Harl::*pointerMF)(void) const;
+
 /* ====== CLASS DECLARATION ====== */
 
 class Harl{
@@ -26,10 +30,14 @@ class Harl{
 				
 				void	complain(std::string level);
 	private:
+				pointerMF	mode[4];
+				
 				void	debug(void);
 				void	info(void);
 				void	warning(void);
 				void	error(void);
+
+				int		convertLevel(const std::string level);
 };
 
 #endif
