c68ff17d3c8887b69c88a706a4e622de18d6c33d
sirui <sirui.qiu01@gmail.com>
2024-02-25 16:19:10 +0100

    refactor[cpp01, ex06]: use convertLevel for switch case

--- cpp01/ex06/src/main.cpp
+++ cpp01/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 15:05:18 by sqiu              #+#    #+#             */
-/*   Updated: 2024/02/25 15:12:47 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/02/25 16:15:00 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,30 @@
 
 int	main(int argc, char **argv){
 	Harl	harl;
+	int		lvl;
 	
 	if (argc != 2){
 		std::cerr << "Invalid program use.\nUsage: " << argv[0];
 		std::cerr << " <log level>" << std::endl;
 		return -1; 
 	}
-	harl.complain(argv[1]);
+	lvl = harl.convertLevel(argv[1]);
+	switch (lvl)
+	{
+		case 0:			
+			harl.complain("DEBUG");
+			break;
+		case 1:
+			harl.complain("INFO");
+			break;
+		case 2:
+			harl.complain("WARNING");
+			break;
+		case 3:
+			harl.complain("ERROR");
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 	return 0;
 }
