d3ee54e80f26d11b64fa023c8cbf48d539bb93b1
sirui <sirui.qiu01@gmail.com>
2024-02-27 12:11:38 +0100

    feat[cpp02, ex02]: add function implementations

---
+++ cpp02/ex02/src/Fixed.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 22:23:44 by sqiu              #+#    #+#             */
+/*   Updated: 2024/02/27 12:10:53 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../inc/Fixed.hpp"
+
+// Default constructor
+Fixed::Fixed(void) : mFixedValue(0){
+	std::cout << "Default constructor called" << std::endl;
+	return;
+}
+
+// Constructor with int parameter
+Fixed::Fixed(const int num) : mFixedValue(num << msFracBits){
+	std::cout << "Int constructor called" << std::endl;
+	return;	
+}
+
+// Constructor with float parameter
+Fixed::Fixed(const float num) : mFixedValue((int)roundf(num * (1 << msFracBits))){
+	std::cout << "Float constructor called" << std::endl;
+	return;
+}
+
+// Copy constructor
+Fixed::Fixed(const Fixed& other){
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+	return;
+}
+
+// Destructor
+Fixed::~Fixed(void){
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+/* MEMBER FUNCTIONS */
+int		Fixed::getRawBits(void) const{
+	return this->mFixedValue;
+}
+
+void	Fixed::setRawBits(int const raw){
+	this->mFixedValue = raw;
+	return;
+}
+
+float	Fixed::toFloat(void) const{
+	return (float)mFixedValue / (1 << msFracBits); // at least one operand has to be float
+}
+
+int		Fixed::toInt(void) const{
+	return (int)roundf((float)mFixedValue / (1 << msFracBits));
+}
+
+/* OPERATOR OVERLOADING */
+// Copy assignment operator
+Fixed&	Fixed::operator= (const Fixed& rhs){
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+		this->mFixedValue = rhs.getRawBits();
+	return *this;
+}
+
+bool	Fixed::operator>(const Fixed& rhs) const{
+	return this->getRawBits() > rhs.getRawBits();
+}
+
+bool	Fixed::operator<(const Fixed& rhs) const{
+	return this->getRawBits() < rhs.getRawBits();
+}
+
+
+
+
+std::ostream&	operator<<(std::ostream& os, const Fixed& fixed){
+	os << fixed.toFloat();
+	return os;
+}
