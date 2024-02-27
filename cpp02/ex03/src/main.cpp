477288c224276f359d512fe07917fabc57e46ec1
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2024-02-27 18:02:57 +0100

    fix[cpp02, ex03]: remove Point assignment

--- cpp02/ex03/src/main.cpp
+++ cpp02/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 19:40:49 by sqiu              #+#    #+#             */
-/*   Updated: 2024/02/27 17:45:31 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/02/27 17:58:29 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,11 @@
 
 bool	bsp( Point const a, Point const b, Point const c, Point const point);
 
-void	test(const Point& a, const Point& b, const Point& c, const Point& p, \
-	const int i){
+void	test(const Point& a, const Point& b, const Point& c, const Point& p){
 	if (bsp(a, b, c, p))
-		std::cout << "Point" << i << " is inside the triangle." << std::endl;
+		std::cout << "Point" << p << " is inside the triangle." << std::endl;
 	else
-		std::cout << "Point" << i << " is outside the triangle." << std::endl;
+		std::cout << "Point" << p << " is outside the triangle." << std::endl;
 	return;
 }
 
@@ -29,25 +28,30 @@ int	main(void){
 	Point	b(5.0, 0.0);
 	Point	c(2.5, 4.0);
 
-	Point*	points = new Point[9];
-	
 	// Points inside the triangle
-	points[0] = Point(2.0, 1.0);
-	points[1] = Point(3.0, 2.0);
-	points[2] = Point(1.0, 3.0);
+	Point	p1(2.0, 1.0);
+	Point	p2(3.0, 2.0);
+	Point	p3(1.0, 3.0);
 
 	// Points outside the triangle
-	points[3] = Point(6.0, 2.0);
-	points[4] = Point(1.0, -1.0);
-	points[5] = Point(3.0, 5.0);
+	Point	p4(6.0, 2.0);
+	Point	p5(1.0, -1.0);
+	Point	p6(3.0, 5.0);
 
 	// Points on the edges of the triangle
-	points[6] = Point(2.5, 0.0);
-	points[7] = Point(0.0, 2.0);
-	points[8] = Point(5.0, 2.0);
+	Point	p7(2.5, 0.0);
+	Point	p8(0.0, 2.0);
+	Point	p9(5.0, 2.0);
 	
-	for (int i = 0; i < 9; i++)
-		test(a, b, c, points[i], i);
-	delete [] points;
+	test(a, b, c, p1);
+	test(a, b, c, p2);
+	test(a, b, c, p3);
+	test(a, b, c, p4);
+	test(a, b, c, p5);
+	test(a, b, c, p6);
+	test(a, b, c, p7);
+	test(a, b, c, p8);
+	test(a, b, c, p9);
+
 	return 0;
 }
