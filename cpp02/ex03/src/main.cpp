f7551d8307388f188315a200cf838f3e1bd8ca9e
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2024-02-27 17:44:55 +0100

    feat[cpp02, ex03]: add main

--- cpp02/ex03/src/main.cpp
+++ cpp02/ex03/src/main.cpp
@@ -6,27 +6,48 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 19:40:49 by sqiu              #+#    #+#             */
-/*   Updated: 2024/02/27 15:37:19 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/02/27 17:44:24 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Fixed.hpp"
+#include "../inc/Point.hpp"
+
+bool	bsp( Point const a, Point const b, Point const c, Point const point);
+
+void	test(const Point& a, const Point& b, const Point& c, const Point& p, \
+	const int i){
+	if (bsp(a, b, c, p))
+		std::cout << "Point" << i << "is inside the triangle." << std::endl;
+	else
+		std::cout << "Point" << i << "is outside the triangle." << std::endl;
+	return;
+}
 
 int	main(void){
-	Fixed a;
-	Fixed const b(Fixed(5.05f) * Fixed(2));
+	Point	a(0.0, 0.0);
+	Point	b(5.0, 0.0);
+	Point	c(2.5, 4.0);
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
+	Point*	points = new Point[9];
 	
-	std::cout << b << std::endl;
+	// Points inside the triangle
+	points[0] = Point(2.0, 1.0);
+	points[1] = Point(3.0, 2.0);
+	points[2] = Point(1.0, 3.0);
+
+	// Points outside the triangle
+	points[3] = Point(6.0, 2.0);
+	points[4] = Point(1.0, -1.0);
+	points[5] = Point(3.0, 5.0);
+
+	// Points on the edges of the triangle
+	points[6] = Point(2.5, 0.0);
+	points[7] = Point(0.0, 2.0);
+	points[8] = Point(5.0, 2.0);
 	
-	std::cout << Fixed::max( a, b ) << std::endl;
-	/* std::cout << Fixed::min( a, b ) << std::endl;
-	std::cout << a-- << std::endl;
-	std::cout << a << std::endl; */
+	for (int i = 0; i < 9; i++)
+		test(a, b, c, points[i], i);
+	delete [] points;
 	return 0;
 }
