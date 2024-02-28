094b89087c07dc4e5d6aead8684b7a5bb2d2eafb
sirui <sirui.qiu01@gmail.com>
2024-02-28 14:15:12 +0100

    feat[cpp03, ex00]: add ClapTrap header

---
+++ cpp03/ex00/inc/ClapTrap.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 13:34:33 by sqiu              #+#    #+#             */
+/*   Updated: 2024/02/28 14:14:31 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+/* ====== LIBRARIES ====== */
+
+#include <string>
+
+/* ====== CLASS DECLARATION ====== */
+
+class ClapTrap{
+	public:
+				ClapTrap(void);
+				ClapTrap(std::string name);
+				ClapTrap(const ClapTrap& other);
+				~ClapTrap(void);
+
+				ClapTrap&	operator=(const ClapTrap& rhs);
+				
+				void		attack(const std::string& target);
+				void		takeDamage(unsigned int amount);
+				void		beRepaired(unsigned int amount);
+
+	private:
+				std::string		mName;
+				int				mHitPoints;
+				int				mEnergyPoints;
+				int				mAttackDamage;
+};
+
+#endif
