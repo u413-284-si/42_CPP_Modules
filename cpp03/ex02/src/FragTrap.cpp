b075e92d8b5fd1a7c650655dede924eec9634297
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2024-03-01 12:29:52 +0100

    feat[cpp03, ex02]: implement functions

---
+++ cpp03/ex02/src/FragTrap.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 15:14:02 by sqiu              #+#    #+#             */
+/*   Updated: 2024/03/01 12:29:08 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/FragTrap.hpp"
+
+//Constructors & Destructors
+
+FragTrap::FragTrap(void) : ClapTrap(){
+	this->mName = "FR4G-TP";
+	this->mHitPoints = 100;
+	this->mEnergyPoints = 100;
+	this->mAttackDamage = 30;
+	std::cout << this->mName << ": Ciao bello!" << std::endl;
+	return;
+}
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name){
+	this->mName = name;
+	this->mHitPoints = 100;
+	this->mEnergyPoints = 100;
+	this->mAttackDamage = 30;
+	std::cout << this->mName << ": Ciao bello!" << std::endl;
+	return;
+}
+
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other){
+	std::cout << this->mName << ": Ciao belli!";
+	std::cout << std::endl;
+	*this = other;
+	return;
+}
+
+// Destructor of base class ClapTrap is automatically called by Destructor
+// of derived class Frag Trap
+FragTrap::~FragTrap(void){
+	std::cout << this->mName << ": mamma mia porca madonna" << std::endl;
+	return;
+}
+
+// Operator overloading
+
+FragTrap&	FragTrap::operator=(const FragTrap& rhs){
+	if (this != &rhs){
+		this->mName = rhs.mName;
+		this->mHitPoints = rhs.mHitPoints;
+		this->mEnergyPoints = rhs.mEnergyPoints;
+		this->mAttackDamage = rhs.mAttackDamage;
+	}
+	return *this;
+}
+
+// Member functions
+
+void	FragTrap::highFivesGuys(void){
+	std::cout << this->mName << ": Who wants to High Five!!!" << std::endl;
+}
