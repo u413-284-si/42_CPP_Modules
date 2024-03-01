9b69bc807f4f8b670501c8129c3db08705fbae30
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2024-03-01 13:18:08 +0100

    feat[cpp03, ex03]: implement functions

---
+++ cpp03/ex03/src/DiamondTrap.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 13:02:07 by sqiu              #+#    #+#             */
+/*   Updated: 2024/03/01 13:16:02 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/DiamondTrap.hpp"
+
+//Constructors & Destructors
+
+DiamondTrap::DiamondTrap(void) : ClapTrap(), ScavTrap(), FragTrap(){
+	this->mName = "DI4MOND-TP";
+	this->mHitPoints = FragTrap::mHitPoints;
+	this->mEnergyPoints = ScavTrap::mEnergyPoints;
+	this->mAttackDamage = FragTrap::mAttackDamage;
+	std::cout << this->mName << ": Momma?" << std::endl;
+	return;
+}
+
+DiamondTrap::DiamondTrap(std::string name) :	ClapTrap(name + "_clap_name"),
+												ScavTrap(name),
+												FragTrap(name){
+	this->mName = name;
+	this->mHitPoints = FragTrap::mHitPoints;
+	this->mEnergyPoints = ScavTrap::mEnergyPoints;
+	this->mAttackDamage = FragTrap::mAttackDamage;
+	std::cout << this->mName << ": Momma?" << std::endl;
+	return;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap& other) :	ClapTrap(other),
+														ScavTrap(other),
+														FragTrap(other){
+	std::cout << this->mName << ": I am Groot";
+	std::cout << std::endl;
+	*this = other;
+	return;
+}
+
+DiamondTrap::~DiamondTrap(void){
+	std::cout << this->mName << ": Bot down." << std::endl;
+	return;
+}
+
+// Operator overloading
+
+DiamondTrap&	DiamondTrap::operator=(const DiamondTrap& rhs){
+	if (this != &rhs){
+		this->mName = rhs.mName;
+		this->mHitPoints = rhs.mHitPoints;
+		this->mEnergyPoints = rhs.mEnergyPoints;
+		this->mAttackDamage = rhs.mAttackDamage;
+	}
+	return *this;
+}
+
+// Member functions
+
+void	DiamondTrap::whoAmI(void){
+	std::cout << "I am " << this->mName << "and my ancestor is";
+	std::cout << ClapTrap::mName << std::endl;
+}
