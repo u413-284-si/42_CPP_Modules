00bfade3eda234f9d45997733b1c16a2455f2071
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2024-03-01 12:04:54 +0100

    feat[cpp03, ex01]: finish up header of derived ScavTrap

--- cpp03/ex01/inc/ScavTrap.hpp
+++ cpp03/ex01/inc/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:03:20 by sqiu              #+#    #+#             */
-/*   Updated: 2024/02/29 15:08:10 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/03/01 11:59:58 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,21 @@
 
 /* ====== LIBRARIES ====== */
 
+#include "../inc/ClapTrap.hpp"
+
 /* ====== CLASS DECLARATION ====== */
 
-class ScavTrap{
+class ScavTrap : virtual public ClapTrap{
 	public:
 				ScavTrap(void);
-				ScavTrap(const ScavTrap&);
+				ScavTrap(std::string name);
+				ScavTrap(const ScavTrap& other);
 				~ScavTrap(void);
 
-				ScavTrap&	operator=(const ScavTrap&);
+				ScavTrap&	operator=(const ScavTrap& rhs);
 
 				void	guardGate(void);
+				void	attack(const std::string& target);
 
 	private:
 	
