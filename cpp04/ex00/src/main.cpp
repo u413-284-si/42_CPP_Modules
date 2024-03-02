34b9b5001d7d1b00fc7c47930e0b90343d2d0f39
sirui <sirui.qiu01@gmail.com>
2024-03-02 14:42:17 +0100

    test[cpp04, ex00]: add main for testing

--- cpp04/ex00/src/main.cpp
+++ cpp04/ex00/src/main.cpp
@@ -6,15 +6,16 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 13:43:19 by sqiu              #+#    #+#             */
-/*   Updated: 2024/03/02 14:10:05 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/03/02 14:40:52 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "../inc/Cat.hpp"
 #include "../inc/Dog.hpp"
+#include "../inc/WrongCat.hpp"
 
-int	main(void){
+void	testSubject(void){
 	const Animal*	meta = new Animal();
 	const Animal*	j = new Dog();
 	const Animal*	i = new Cat();
@@ -28,5 +29,26 @@ int	main(void){
 	delete meta;
 	delete i;
 	delete j;
+	return;
+}
+
+void	testWrongAnimal(void){
+	const WrongAnimal*	bubu = new WrongCat();
+	WrongAnimal a;
+
+	std::cout << bubu->getType() << std::endl;
+	bubu->makeSound();
+	std::cout << a.getType() << std::endl;
+	a.makeSound();
+
+	delete bubu;
+	return;
+}
+
+int	main(void){
+	std::cout << "\n* TEST SUBJECT *\n" << std::endl;
+	testSubject();
+	std::cout << "\n* TEST WRONGCAT *\n" << std::endl;
+	testWrongAnimal();
 	return 0;	
 }
