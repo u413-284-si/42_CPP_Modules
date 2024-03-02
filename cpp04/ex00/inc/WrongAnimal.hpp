3a544cf43b451d3165de1e8d9ee7a2aa119701ec
sirui <sirui.qiu01@gmail.com>
2024-03-02 14:04:37 +0100

    feat[cpp04, ex00]: add WrongAnimal class

---
+++ cpp04/ex00/inc/WrongAnimal.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 18:15:03 by sqiu              #+#    #+#             */
+/*   Updated: 2024/03/02 13:53:57 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGANIMAL_HPP
+# define WRONGANIMAL_HPP
+
+/* ====== LIBRARIES ====== */
+
+#include <iostream>
+
+/* ====== CLASS DECLARATION ====== */
+
+class WrongAnimal{
+	public:
+				WrongAnimal(void);
+				WrongAnimal(const std::string& typus);
+				WrongAnimal(const WrongAnimal& other);
+				~WrongAnimal(void);
+
+				WrongAnimal&		operator=(const WrongAnimal& rhs);
+				const std::string&	getType(void) const;
+				virtual void		makeSound(void) const;
+
+	protected:
+				std::string	_type;
+};
+
+#endif
