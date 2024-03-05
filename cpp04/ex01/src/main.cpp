b304541c24d3d20e59202ebdf0adaa89270298c0
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2024-03-05 17:56:42 +0100

    test[cpp04, ex01]: add subject tests

--- cpp04/ex01/src/main.cpp
+++ cpp04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 13:43:19 by sqiu              #+#    #+#             */
-/*   Updated: 2024/03/02 14:40:52 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/03/05 17:55:34 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,30 @@
 #include "../inc/WrongCat.hpp"
 
 void	testSubject(void){
-	const Animal*	meta = new Animal();
-	const Animal*	j = new Dog();
-	const Animal*	i = new Cat();
-
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound();
-	j->makeSound();
-	meta->makeSound();
-
-	delete meta;
-	delete i;
-	delete j;
+	Animal*	animals[10];
+	
+	for (int i = 0; i < 5; i++){
+		animals[i] = new Dog();
+		animals[i + 5] = new Cat();
+	}
+	for (int i = 0; i < 10; i++)
+		delete animals[i];
 	return;
 }
 
-void	testWrongAnimal(void){
-	const WrongAnimal*	bubu = new WrongCat();
-	WrongAnimal a;
-
-	std::cout << bubu->getType() << std::endl;
-	bubu->makeSound();
-	std::cout << a.getType() << std::endl;
-	a.makeSound();
-
-	delete bubu;
+void	testSubject2(void){
+	const Animal*	j = new Dog();
+	const Animal*	i = new Cat();
+	
+	delete j;
+	delete i;
 	return;
 }
 
 int	main(void){
 	std::cout << "\n* TEST SUBJECT *\n" << std::endl;
 	testSubject();
-	std::cout << "\n* TEST WRONGCAT *\n" << std::endl;
-	testWrongAnimal();
+	std::cout << "\n* TEST SUBJECT2 *\n" << std::endl;
+	testSubject2();
 	return 0;	
 }
