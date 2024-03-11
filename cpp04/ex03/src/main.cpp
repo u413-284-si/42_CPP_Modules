321ce5f5ef7547f6001081d1e564d60e6ba43d50
sirui <sirui.qiu01@gmail.com>
2024-03-11 15:35:02 +0100

    test[cpp04, ex03]: add Character tests

--- cpp04/ex03/src/main.cpp
+++ cpp04/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 14:48:38 by sqiu              #+#    #+#             */
-/*   Updated: 2024/03/11 14:52:22 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/03/11 15:34:03 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,57 @@ void	testSubject(void){
 	delete src;
 }
 
+void	testCharacter(void){
+	ICharacter*	dude = new Character("Jamal");
+	ICharacter*	dudette = new Character("Teesha");
+	AMateria*	cure1 = new Cure();
+	AMateria*	cure2 = new Cure();
+	AMateria*	ice1 = new Ice();
+
+	dude->use(1, *dudette);
+	
+	dude->equip(new Cure());
+	dude->equip(new Ice());
+	dude->equip(new Ice());
+	dude->equip(cure1);
+	dude->equip(cure2);
+	dude->equip(ice1);
+	
+	dude->use(0, *dudette);
+	dude->use(1, *dudette);
+	dude->use(2, *dudette);
+	dude->use(3, *dudette);
+	
+	dude->unequip(3);
+	dude->use(3, *dudette);
+	dude->equip(new Cure());
+	dude->use(3, *dudette);
+
+	AMateria * materia = dude->getMateria(0);
+	dude->unequip(0);
+	dude->use(0, *dudette);
+	dude->equip(new Cure());
+	dude->use(0, *dudette);
+
+	delete materia;
+	delete cure1;
+	delete cure2;
+	delete ice1;
+	delete dudette;
+	delete dude;
+	return;
+}
+
+void	testMateriaSource(void){
+	
+}
+
 int	main(void){
 	std::cout << "\n* TEST SUBJECT *\n" << std::endl;
 	testSubject();
+	std::cout << "\n* TEST CHARACTER *\n" << std::endl;
+	testCharacter();
+	std::cout << "\n* TEST MATERIASOURCE *\n" << std::endl;
+	testMateriaSource();
 	return 0;
 }
