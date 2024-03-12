e0edd6a3cef46f635033cf0157ecffc492295d7d
sirui <sirui.qiu01@gmail.com>
2024-03-13 00:25:40 +0100

    feat[cpp05, ex00]: add main with tests

---
+++ cpp05/ex00/src/main.cpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 23:57:29 by sqiu              #+#    #+#             */
+/*   Updated: 2024/03/13 00:21:54 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Bureaucrat.hpp"
+
+void	testNoException(void){
+	Bureaucrat	bibi("bibi", 3);
+	
+	std::cout << bibi << std::endl;
+	bibi.incrGrade();
+	std::cout << bibi << std::endl;
+	bibi.decrGrade();
+	std::cout << bibi << std::endl;
+	return;	
+}
+
+// Boundary value testing
+
+void	testConstructorHighException(void){
+	Bureaucrat	bubu("bubu", -1);
+	return;
+}
+
+void	testConstructorLowException(void){
+	Bureaucrat	bubu("bubu", 151);
+	return;
+}
+
+void	testIncrementHighException(void){
+	Bureaucrat	bubu("bubu", 1);
+
+	bubu.incrGrade();
+	return;
+}
+
+void	testDecrementLowException(void){
+	Bureaucrat	bubu("bubu", 150);
+
+	bubu.decrGrade();
+	return;
+}
+
+int	main(void){
+	std::cout << "\n* TEST NO EXCEPTION *\n" << std::endl;
+	try{
+		testNoException();
+	}
+	catch(std::exception& e){
+		std::cerr << e.what() << std::endl;
+	}
+	
+	std::cout << "\n* TEST CONSTRUCTOR HIGH EXCEPTION *\n" << std::endl;
+	try{
+		testConstructorHighException();
+	}
+	catch(std::exception& e){
+		std::cerr << e.what() << std::endl;
+	}
+	
+	std::cout << "\n* TEST CONSTRUCTOR LOW EXCEPTION *\n" << std::endl;
+	try{
+		testConstructorLowException();
+	}
+	catch(std::exception& e){
+		std::cerr << e.what() << std::endl;
+	}
+	
+	std::cout << "\n* TEST INCREMENT HIGH EXCEPTION *\n" << std::endl;
+	try{
+		testIncrementHighException();
+	}
+	catch(std::exception& e){
+		std::cerr << e.what() << std::endl;
+	}
+	
+	std::cout << "\n* TEST DECREMENT LOW EXCEPTION *\n" << std::endl;
+	try{
+		testDecrementLowException();
+	}
+	catch(std::exception& e){
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
+}
