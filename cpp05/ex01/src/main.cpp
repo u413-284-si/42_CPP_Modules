e3c4cb980725f54bee48bfc874dd820158b1b032
sirui <sirui.qiu01@gmail.com>
2024-03-14 17:56:03 +0100

    feat[cpp05, ex01]: add Form test

--- cpp05/ex01/src/main.cpp
+++ cpp05/ex01/src/main.cpp
@@ -6,11 +6,12 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 23:57:29 by sqiu              #+#    #+#             */
-/*   Updated: 2024/03/13 00:26:08 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/03/14 17:47:25 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Bureaucrat.hpp"
+#include "../inc/Form.hpp"
 
 void	testNoException(void){
 	Bureaucrat	bibi("bibi", 3);
@@ -51,45 +52,60 @@ void	testDecrementLowException(void){
 	return;
 }
 
+void	testForm(void){
+	Form		form("test", 7, 8);
+	Bureaucrat	bubu("bubu", 8);
+
+	std::cout << form << std::endl;
+	bubu.signForm(form);
+	bubu.incrGrade();
+	bubu.signForm(form);
+	std::cout << form << std::endl;
+	return;
+}
+
 int	main(void){
-	std::cout << "\n* TEST NO EXCEPTION *\n" << std::endl;
+	/* std::cout << "\n* TEST NO EXCEPTION *\n" << std::endl;
 	try{
 		testNoException();
 	}
 	catch(std::exception& e){
 		std::cerr << e.what() << std::endl;
-	}
+	} */
 	
-	std::cout << "\n* TEST CONSTRUCTOR HIGH EXCEPTION *\n" << std::endl;
+	/* std::cout << "\n* TEST CONSTRUCTOR HIGH EXCEPTION *\n" << std::endl;
 	try{
 		testConstructorHighException();
 	}
 	catch(std::exception& e){
 		std::cerr << e.what() << std::endl;
-	}
+	} */
 	
-	std::cout << "\n* TEST CONSTRUCTOR LOW EXCEPTION *\n" << std::endl;
+	/* std::cout << "\n* TEST CONSTRUCTOR LOW EXCEPTION *\n" << std::endl;
 	try{
 		testConstructorLowException();
 	}
 	catch(std::exception& e){
 		std::cerr << e.what() << std::endl;
-	}
+	} */
 	
-	std::cout << "\n* TEST INCREMENT HIGH EXCEPTION *\n" << std::endl;
+	/* std::cout << "\n* TEST INCREMENT HIGH EXCEPTION *\n" << std::endl;
 	try{
 		testIncrementHighException();
 	}
 	catch(std::exception& e){
 		std::cerr << e.what() << std::endl;
-	}
+	} */
 	
-	std::cout << "\n* TEST DECREMENT LOW EXCEPTION *\n" << std::endl;
+	/* std::cout << "\n* TEST DECREMENT LOW EXCEPTION *\n" << std::endl;
 	try{
 		testDecrementLowException();
 	}
 	catch(std::exception& e){
 		std::cerr << e.what() << std::endl;
-	}
+	} */
+	
+	std::cout << "\n* TEST FORM *\n" << std::endl;
+	testForm();
 	return 0;
 }
