2735f4ee4f1891620d4168006eb80b669a226109
sirui <sirui.qiu01@gmail.com>
2024-03-27 19:19:28 +0100

    feat[cpp06, ex00]: feedback correct input format

--- cpp06/ex00/src/main.cpp
+++ cpp06/ex00/src/main.cpp
@@ -6,15 +6,17 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 18:14:40 by sqiu              #+#    #+#             */
-/*   Updated: 2024/03/27 18:17:41 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/03/27 19:18:29 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ScalarConverter.hpp"
 
 int	main(int argc, char** argv){
-	if (argc != 2)
-		std::cerr << "invalid number of arguments" << std::endl;
+	if (argc != 2){
+		std::cerr << "invalid number of arguments\n";
+		std::cerr << "expected: ./conv <input>" << std::endl;
+	}
 	else
 		ScalarConverter::convert(argv[1]);
 	return 0;
