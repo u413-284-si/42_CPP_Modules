5d8b374d2abec70cc369801c0dcf273d575a6970
sirui <sirui.qiu01@gmail.com>
2024-04-05 15:16:51 +0200

    feat[cpp07, ex01]: add test arrays to main

--- cpp07/ex01/src/main.cpp
+++ cpp07/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 14:31:26 by sqiu              #+#    #+#             */
-/*   Updated: 2024/04/04 15:43:13 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/04/05 15:02:48 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,19 @@ int	main(void){
 	float		arr_float[ARR_SIZE] = {1.231f, 2.244f, 3.363f};
 	double		arr_double[ARR_SIZE] = {1.221, 2.233, 3.354};
 	std::string	arr_str[ARR_SIZE] = {"oans", "zwo", "dree"};
+	const char	*arr_strC[ARR_SIZE] = {"un", "deux", "trois"};
+	char		arr_char[ARR_SIZE] = {'e', 't', '3'};
 
 	iter(arr_int, ARR_SIZE, &print);
+	std::cout << std::endl;
 	iter(arr_float, ARR_SIZE, &print);
+	std::cout << std::endl;
 	iter(arr_double, ARR_SIZE, &print);
+	std::cout << std::endl;
 	iter(arr_str, ARR_SIZE, &print);
+	std::cout << std::endl;
+	iter(arr_strC, ARR_SIZE, &print);
+	std::cout << std::endl;
+	iter(arr_char, ARR_SIZE, &print);
 	return 0;
 }
