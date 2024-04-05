5a0eff9e22b35e93443ec689ed1186fec2717bb1
sirui <sirui.qiu01@gmail.com>
2024-04-05 19:31:47 +0200

    feat[cpp07, ex02]: add template class Array declarations

---
+++ cpp07/ex02/inc/Array.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/05 15:23:02 by sqiu              #+#    #+#             */
+/*   Updated: 2024/04/05 18:25:19 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+/* ====== LIBRARIES ====== */
+
+#include <cstdlib>
+
+/* ====== DECLARATIONS ====== */
+
+template <typename T>
+class Array{
+	public:
+				Array<T>(void);
+				Array<T>(const unsigned int n);
+				Array<T>(const Array& other);
+				~Array<T>(void);
+
+				Array<T>&		operator=(const Array<T>& rhs);
+				T&				operator[](const unsigned int n);
+
+				unsigned int	size(void) const;
+	private:	
+				unsigned int	_n;
+				T				*_arr;
+};
+
+#include "../src/Array.tpp"
