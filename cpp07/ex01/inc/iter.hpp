becbeb56dd67f884466ffb416836e56952314731
sirui <sirui.qiu01@gmail.com>
2024-04-05 15:18:19 +0200

    refactor[cpp07, ex01]: remove additional template with non const func parameter

--- cpp07/ex01/inc/iter.hpp
+++ cpp07/ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:41:05 by sqiu              #+#    #+#             */
-/*   Updated: 2024/04/04 14:11:51 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/04/05 15:16:11 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,7 @@
 /* ====== DEFINITIONS ====== */
 
 template <typename T>
-void	iter(T *arr, const size_t& size, void (* const func)(T&)){
-	for (size_t i = 0; i < size; i++)
-		func(arr[i]);
-	return;
-}
-
-template <typename T>
-void	iter(T *arr, const size_t& size, void (* const func)(const T&)){
+void	iter(T *arr, const size_t size, void (* const func)(const T&)){
 	for (size_t i = 0; i < size; i++)
 		func(arr[i]);
 	return;
