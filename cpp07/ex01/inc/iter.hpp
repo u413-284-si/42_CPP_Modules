0e094997910da3bf6a6c5859d3b35b1d2fe65b5d
sirui <sirui.qiu01@gmail.com>
2024-04-05 22:00:27 +0200

    feat[cpp07, ex01]: handling const and non-const arrays

--- cpp07/ex01/inc/iter.hpp
+++ cpp07/ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:41:05 by sqiu              #+#    #+#             */
-/*   Updated: 2024/04/05 15:16:11 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/04/05 21:51:08 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,17 @@
 
 /* ====== DEFINITIONS ====== */
 
+// Handle const arrays
 template <typename T>
-void	iter(T *arr, const size_t size, void (* const func)(const T&)){
+void	iter(const T *arr, const size_t size, void (* const func)(const T&)){
+	for (size_t i = 0; i < size; i++)
+		func(arr[i]);
+	return;
+}
+
+// Handle non-const arrays
+template <typename T>
+void	iter(T *arr, const size_t size, void (* const func)(T&)){
 	for (size_t i = 0; i < size; i++)
 		func(arr[i]);
 	return;
