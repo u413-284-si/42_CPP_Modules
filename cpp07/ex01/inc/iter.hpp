5bebe198623aa35006a0ff00a99bbb82fe52baf7
sirui <sirui.qiu01@gmail.com>
2024-04-09 19:02:45 +0200

    fix[cpp07, ex01]: add check against NULL pointer

--- cpp07/ex01/inc/iter.hpp
+++ cpp07/ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:41:05 by sqiu              #+#    #+#             */
-/*   Updated: 2024/04/05 21:51:08 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/04/09 19:01:15 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@
 // Handle const arrays
 template <typename T>
 void	iter(const T *arr, const size_t size, void (* const func)(const T&)){
+	if (!arr)
+		return;
 	for (size_t i = 0; i < size; i++)
 		func(arr[i]);
 	return;
@@ -29,6 +31,8 @@ void	iter(const T *arr, const size_t size, void (* const func)(const T&)){
 // Handle non-const arrays
 template <typename T>
 void	iter(T *arr, const size_t size, void (* const func)(T&)){
+	if (!arr)
+		return;
 	for (size_t i = 0; i < size; i++)
 		func(arr[i]);
 	return;
