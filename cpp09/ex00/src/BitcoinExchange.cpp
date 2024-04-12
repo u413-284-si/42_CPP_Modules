ba18ed59916bc6a488d697cf9a8d2edfc58df90b
sqiu <sqiu@student.42vienna.com>
2024-04-12 17:30:51 +0200

    feat[cpp09, ex00]: implement check for date and rate

--- cpp09/ex00/src/BitcoinExchange.cpp
+++ cpp09/ex00/src/BitcoinExchange.cpp
@@ -34,7 +34,7 @@ BitcoinExchange::BitcoinExchange(void){
 			this->_xChangeRate.insert(std::make_pair(date, rate));
 		}
 		catch(std::exception& e){
-			std::cerr << "Invalid line " << lineCount << ": " e.what() << std::endl;
+			std::cerr << "Invalid line " << lineCount << ": " << e.what() << std::endl;
 		}
 	}
 	data.close();
@@ -65,29 +65,100 @@ int		BitcoinExchange::checkHeader(const std::string& line) const{
 }
 
 void	BitcoinExchange::checkLine(const std::string& line, time_t& date, double& rate) const{
-	if (line.find(',') == std::string::npos)
+	std::size_t			pos;
+	std::string	strDate;
+	std::string	strRate;
+
+	if (line.empty())
+		throw std::invalid_argument("empty line detected");
+	pos = line.find(','); 
+	if (pos == std::string::npos)
 		throw std::invalid_argument("no delimiter found");
-	checkDate(line, date);
-	checkRate(line, rate);
+	strDate = line.substr(0, pos++);
+	checkDate(strDate, date);
+	strRate = line.substr(pos, std::string::npos - pos);
+	checkRate(strRate, rate);
 	return;
 }
 
-void	BitcoinExchange::checkDate(const std::string& line, time_t& date) const{
-	std::size_t	pos = line.find(',');
-	std::string	date = line.substr(0, pos);
-	char		*szy;
-	char		*szm;
-	char		*szd;
-	double		year = stod(line.substr(0, 4).c_str(), &szy);
-	double		month = stod(line.substr(5, 2).c_str(), &szm);
-	double		day = stod(line.substr(8, 2).c_str(), &szd);
-	
-
-	if (date.length() != 10)
-		throw std::invalid_argument("date length invalid");
-	if (date.)
+/* Any year that is evenly divisible by 4 is a leap year. However, there is still
+a small error that must be accounted for. To eliminate this error, the Gregorian
+calendar stipulates that a year that is evenly divisible by 100 is a leap year only
+if it is also evenly divisible by 400. */
+bool	BitcoinExchange::isLeapYear(const int year) const{
+	if (year % 4 == 0){
+		if (year % 100 == 0){
+			if (year % 400 == 0)
+				return true;
+			else
+				return false;
+		}
+		else
+			return true;
+	}
+	else
+		return false;
 }
 
-void	BitcoinExchange::checkRate(const std::string& line, double& rate){
+void	BitcoinExchange::checkDate(const std::string& strDate, time_t& date) const{
+	char		*endy;
+	char		*endm;
+	char		*endd;
+	double		year;
+	double		month;
+	double		day;
+	std::tm		time;
+	const unsigned	largeMonths = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7) | (1 << 8) | (1 << 10) | (1 << 12);
+	const unsigned	smallMonths = (1 << 4) | (1 << 6) | (1 << 9) | (1 << 11);
 
+	if (strDate.length() != 10)
+		throw std::invalid_argument("date length invalid");
+	year = strtod(strDate.c_str(), &endy);
+	if (*endy != '-')
+		throw std::invalid_argument("invalid format");
+	endy++;
+	month = strtod(endy, &endm);
+	if (*endm != '-')
+		throw std::invalid_argument("invalid format");
+	endm++;
+	day = strtod(endm, &endd);
+	if (*endd != ',')
+		throw std::invalid_argument("invalid format");
+
+	if (month < 1 || month > 12)
+		throw std::invalid_argument("invalid month");
+	if (day < 1)
+		throw std::invalid_argument("invalid day < 1");
+
+	if ((1 << static_cast<int>(month)) & largeMonths){
+		if (day > 31)
+			throw std::invalid_argument("invalid day > 31");
+	}
+	else if ((1 << static_cast<int>(month)) & smallMonths){
+		if (day > 30)
+			throw std::invalid_argument("invalid day > 30");
+	}
+	else{
+		if (isLeapYear(year)){
+			if (day > 29)
+				throw std::invalid_argument("invalid day > 29");
+		}
+		else{
+			if (day > 28)
+				throw std::invalid_argument("invalid day > 28");
+		}
+	}
+	time.tm_year = year;
+	time.tm_mon = month;
+	time.tm_mday = day;
+	date = mktime(&time);
+}
+
+void	BitcoinExchange::checkRate(const std::string& strRate, double& rate) const{
+	char	*endptr;
+	if (strRate.empty())
+		throw std::invalid_argument("no rate given");
+	rate = strtod(strRate.c_str(), &endptr);
+	if (*endptr != '\0')
+		throw std::invalid_argument("invalid rate format");
 }
