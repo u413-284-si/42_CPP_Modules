56644317e8d77fb47146d09b21ba39f3f6c886e0
sirui <sirui.qiu01@gmail.com>
2024-04-17 13:28:21 +0200

    feat[cpp09, ex00]: add parseInput() to main

--- cpp09/ex00/src/main.cpp
+++ cpp09/ex00/src/main.cpp
@@ -6,22 +6,21 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 16:19:22 by sqiu              #+#    #+#             */
-/*   Updated: 2024/04/08 16:19:43 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/04/17 13:22:35 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/BitcoinExchange.hpp"
 
 int	main(int argc, char **argv){
-	/* if (argc != 2){
+	if (argc != 2){
 		std::cerr << "Error: insufficient arguments." << std::endl;
 		return 1;
-	} */
-	(void)argc;
-	(void)argv;
+	}
 	try{
 		BitcoinExchange	btc;
-		btc.printData();
+		btc.parseInput(argv[1]);
+		//btc.printData();
 	}
 	catch(std::exception& e){
 		std::cerr << "Error: " << e.what() << std::endl;
