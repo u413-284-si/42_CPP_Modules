540c55483061e304b686d3bdee401dab9496aa6d
Sirui <sirui.qiu01@gmail.com>
2024-04-19 14:46:42 +0200

    fix[cpp09, ex01]: return if insufficient args

--- cpp09/ex01/src/main.cpp
+++ cpp09/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:30:55 by sqiu              #+#    #+#             */
-/*   Updated: 2024/04/19 14:39:18 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/04/19 14:45:55 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int argc, char **argv){
 	if (argc != 2){
 		std::cout << "Error: insufficient arguments\n";
 		std::cout << "expected: ./RPN <operation string>" << std::endl;
+		return -1;
 	}
 	try{
 		calculator.parseInput(argv[1]);
