56b29488cbfd1346c9c7490258dad4c9cf56fc51
Sirui <sirui.qiu01@gmail.com>
2024-04-19 15:44:47 +0200

    test[cpp09, ex01]: add unit tests

--- cpp09/ex01/src/main.cpp
+++ cpp09/ex01/src/main.cpp
@@ -6,14 +6,61 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:30:55 by sqiu              #+#    #+#             */
-/*   Updated: 2024/04/19 14:45:55 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/04/19 15:42:31 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/RPN.hpp"
 
-int	main(int argc, char **argv){
-	RPN	calculator;
+void	unitTester(const std::string& name, size_t total,\
+			const std::pair<std::string, double> tests[]){
+	RPN 	calculator;
+	size_t	passed = 0;
+	int		result;
+	
+	std::cout << "* " << name << " *\n";
+	for (size_t i = 0; i < total; i++){
+		std::cout << "Test [" << i << "]: " << tests[i].first << std::endl;
+		try{
+			calculator.parseInput(tests[i].first);
+			result = calculator.getResult();
+			if (result != tests[i].second){
+				std::cerr << RED << "FAILURE\nexpected: " << tests[i].second;
+				std::cerr << "; calculated: " << calculator.getResult();
+				std::cerr << RESET << std::endl;
+			}
+			else{
+				std::cout << GREEN << "SUCCESS\nresult: " << result;
+				std::cout << RESET << std::endl;
+				passed++;
+			}
+		}
+		catch(std::exception& e){
+			std::cerr << BLUE << "Error: " << e.what() << RESET << std::endl;
+		}
+	}
+	std::cout << CYAN << passed << " of " << total << " passed" << RESET;
+	std::cout << std::endl;
+	return;
+}
+
+void	testValid(void){
+	std::pair<std::string, double>	tests[] = {
+		std::make_pair("8 9 * 9 - 9 - 9 - 4 - 1 +", 42),
+		std::make_pair("7 7 * 7 -", 42),
+		std::make_pair("1 2 * 2 / 2 * 2 4 - +", 0),
+		std::make_pair("1 2 * 2 / 2 + 5 * 6 - 1 3 * - 4 5 * * 8 /", 15),
+	};
+	unitTester("VALID OPERATIONS", sizeof(tests) / sizeof(tests[0]), tests);
+	return;
+}
+
+int		main(int argc, char **argv){
+	// For testing
+	(void)argc;
+	(void)argv;
+	testValid();
+/* 	RPN	calculator;
 
 	if (argc != 2){
 		std::cout << "Error: insufficient arguments\n";
@@ -25,7 +72,7 @@ int	main(int argc, char **argv){
 		std::cout << calculator.getResult() << std::endl;
 	}
 	catch(std::exception& e){
-		std::cout << "Error: " << e.what() << std::endl;
-	}
+		std::cout << BLUE << "Error: " << e.what() << RESET << std::endl;
+	} */
 	return 0;
 }
