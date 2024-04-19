ceb919c3ea31e316e1ce58919911c9bceb5b2d64
Sirui <sirui.qiu01@gmail.com>
2024-04-19 20:56:42 +0200

    test[cpp09, ex01]: add invalid tests

--- cpp09/ex01/src/main.cpp
+++ cpp09/ex01/src/main.cpp
@@ -6,18 +6,18 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:30:55 by sqiu              #+#    #+#             */
-/*   Updated: 2024/04/19 16:03:54 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/04/19 20:54:59 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/RPN.hpp"
 
-void	unitTester(const std::string& name, size_t total,\
+void	runTests(const std::string& name, size_t total,\
 			const std::pair<std::string, double> tests[]){
 	size_t	passed = 0;
 	int		result;
 	
-	std::cout << "* " << name << " *\n";
+	std::cout << "\n* " << name << " *\n";
 	for (size_t i = 0; i < total; i++){
 		std::cout << "Test [" << i << "]: " << tests[i].first << std::endl;
 		{
@@ -51,18 +51,44 @@ void	testValid(void){
 		std::make_pair("8 9 * 9 - 9 - 9 - 4 - 1 +", 42),
 		std::make_pair("7 7 * 7 -", 42),
 		std::make_pair("1 2 * 2 / 2 * 2 4 - +", 0),
+		std::make_pair("9 8 * 4 * 4 / 2 + 9 - 8 - 8 - 1 - 6 -", 42),
 		std::make_pair("1 2 * 2 / 2 + 5 * 6 - 1 3 * - 4 5 * * 8 /", 15),
+		std::make_pair("4      6 *      2          +    ", 26),
+		std::make_pair("3 9 - 9 - 3 *", -45),
 	};
-	unitTester("VALID OPERATIONS", sizeof(tests) / sizeof(tests[0]), tests);
+	runTests("VALID OPERATIONS", sizeof(tests) / sizeof(tests[0]), tests);
+	return;
+}
+
+void	testInvalid(void){
+	std::pair<std::string, double>	tests[] = {
+		std::make_pair("(1 + 1)", 42),
+		std::make_pair("7.0 7.0 * 7.0 -", 42),
+		std::make_pair("7 7 * 42 -", 42),
+		std::make_pair("1 z +", 42),
+		std::make_pair("1 1 ", 42),
+		std::make_pair("1 0 /", 42),
+		std::make_pair("1 -", 42),
+		std::make_pair("1 1 2 ++", 42),
+		std::make_pair("1 1 2 +", 42),
+		std::make_pair("", 42),
+	};
+	runTests("INVALID OPERATIONS", sizeof(tests) / sizeof(tests[0]), tests);
+	return;
+}
+
+void	unitTester(void){
+	testValid();
+	testInvalid();
 	return;
 }
 
 int		main(int argc, char **argv){
-	// For testing
+	/* // For testing
 	(void)argc;
 	(void)argv;
-	testValid();
-/* 	RPN	calculator;
+	unitTester(); */
+	RPN	calculator;
 
 	if (argc != 2){
 		std::cout << "Error: insufficient arguments\n";
@@ -75,6 +101,6 @@ int		main(int argc, char **argv){
 	}
 	catch(std::exception& e){
 		std::cout << BLUE << "Error: " << e.what() << RESET << std::endl;
-	} */
+	}
 	return 0;
 }
