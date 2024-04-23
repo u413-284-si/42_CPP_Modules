8c713450079a756ac0af31a4a3656f63343d684b
Sirui <sirui.qiu01@gmail.com>
2024-04-23 17:00:00 +0200

    feat[cpp09, ex02]: add header

---
+++ cpp09/ex02/inc/PmergeMe.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 11:43:01 by sqiu              #+#    #+#             */
+/*   Updated: 2024/04/22 20:47:11 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+/* ====== LIBRARIES ====== */
+
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+#include <list>
+#include <vector>
+
+/* ====== DEFINITIONS ====== */
+
+# define VERBOSE 0
+
+// ANSI color codes
+#define RESET   "\033[0m"
+#define RED     "\033[31m"    
+#define GREEN   "\033[32m"    
+#define YELLOW  "\033[33m"    
+#define BLUE    "\033[34m"    
+#define MAGENTA "\033[35m"    
+#define CYAN    "\033[36m"    
+#define WHITE   "\033[37m"  
+
+/* ====== CLASS DECLARATION ====== */
+
+class PmergeMe{
+	public:
+
+	private:
+
+};
