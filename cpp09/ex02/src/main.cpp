04a20965146cd2c55a6341de5dd281ba03c4fb31
sirui <sirui.qiu01@gmail.com>
2024-04-25 18:28:16 +0200

    fix[cpp09, ex02]: fix error output format

--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 17:57:02 by sqiu              #+#    #+#             */
-/*   Updated: 2024/04/25 18:04:59 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/04/25 18:27:11 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv){
 	
 	if (argc < 2){
 		std::cout << RED << "Error: insufficient arguments\nexpected: ";
-		std::cout << argv[0] << "<list of positive integers>" << RESET << std::endl;
+		std::cout << argv[0] << " <list of positive integers>" << RESET << std::endl;
 		return 1;
 	}
 	try{
