506ac7f6364d79eb10ae9ab82dce6764c5844998
sirui <sirui.qiu01@gmail.com>
2024-05-17 16:20:04 +0200

    test[cpp09, ex02]: add test random and invalid input

--- cpp09/ex02/src/test.cpp
+++ cpp09/ex02/src/test.cpp
@@ -6,12 +6,11 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 10:37:32 by sqiu              #+#    #+#             */
-/*   Updated: 2024/05/17 12:17:13 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/05/17 16:18:04 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/PmergeMe.hpp"
-#include <cstring>
 
 // Function to convert std::vector<std::string> to char**
 char** convertToCharArray(const std::vector<std::string>& vec) {
@@ -50,30 +49,31 @@ void	runTests(const std::string& name, size_t total, const std::pair<std::vector
 			char		**testArr = convertToCharArray(tests[i].first);
 			try{
 					p.checkInput(testArr);
+			
+					// Check handling with vector
+					if (p.handleInputWithVector(testArr) > tests[i].second){
+						std::cerr << YELLOW << "Warning: More comparisons required than stipulated by theory\n" << RESET;
+					}
+					std::vector<int>	tmpVector = p.getVector();
+					if (std::is_sorted(tmpVector.begin(), tmpVector.end()))
+						std::cout << GREEN << "\nSUCCESS!\n" << RESET << std::endl;
+					else
+						std::cerr << RED << "\nFAILURE - numbers are not sorted\n" << RESET;
+
+					// Check handling with deque
+					if (p.handleInputWithDeque(testArr) > tests[i].second){
+						std::cerr << YELLOW << "Warning: More comparisons required than stipulated by theory\n";
+					}
+					std::deque<int>	tmpDeque = p.getDeque();
+					if (std::is_sorted(tmpDeque.begin(), tmpDeque.end()))
+						std::cout << GREEN << "\nSUCCESS!\n" << RESET << std::endl;
+					else
+						std::cerr << RED << "\nFAILURE - numbers are not sorted\n" << RESET;
+					freeCharArray(testArr, tests[i].first.size());
 			}
 			catch(std::exception& e){
 				std::cout << RED << "Error: " << e.what() << RESET << std::endl;
 			}
-			// Check handling with vector
-			if (p.handleInputWithVector(testArr) > tests[i].second){
-				std::cerr << YELLOW << "Warning: More comparisons required than stipulated by theory\n" << RESET;
-			}
-			std::vector<int>	tmpVector = p.getVector();
-			if (std::is_sorted(tmpVector.begin(), tmpVector.end()))
-				std::cout << GREEN << "\nSUCCESS!\n" << RESET << std::endl;
-			else
-				std::cerr << RED << "\nFAILURE - numbers are not sorted\n" << RESET;
-
-			// Check handling with deque
-			if (p.handleInputWithDeque(testArr) > tests[i].second){
-				std::cerr << YELLOW << "Warning: More comparisons required than stipulated by theory\n";
-			}
-			std::deque<int>	tmpDeque = p.getDeque();
-			if (std::is_sorted(tmpDeque.begin(), tmpDeque.end()))
-				std::cout << GREEN << "\nSUCCESS!\n" << RESET << std::endl;
-			else
-				std::cerr << RED << "\nFAILURE - numbers are not sorted\n" << RESET;
-			freeCharArray(testArr, tests[i].first.size());	
 		}
 	}
 	return;
@@ -110,7 +110,40 @@ void	testValid(void){
 	return;
 }
 
+void	testInvalid(void){
+	std::pair<std::vector<std::string>, int>	tests[] = {
+		std::make_pair(std::vector<std::string>{"prog", "3", ""}, 0),
+		std::make_pair(std::vector<std::string>{"prog", "t"}, 0),
+		std::make_pair(std::vector<std::string>{"prog", "8.2", "2"}, 0),
+		std::make_pair(std::vector<std::string>{"prog", "-9", "3"}, 0),
+		std::make_pair(std::vector<std::string>{"prog", "2147483648", "3"}, 0),
+		std::make_pair(std::vector<std::string>{"prog", "-2147483649", "3"}, 0),
+		std::make_pair(std::vector<std::string>{"prog", "3#", "9", "35"}, 0),
+		std::make_pair(std::vector<std::string>{"prog", "3.0f", "9", "35"}, 0),
+		std::make_pair(std::vector<std::string>{"prog", "3.0", "9", "35"}, 0),
+		std::make_pair(std::vector<std::string>{"prog", "3,0", "9", "35"}, 0),
+	};
+	runTests("INVALID TEST CASES", sizeof(tests) / sizeof(tests[0]), tests);
+	return;
+}
+
+void	testRandom(void){
+	std::srand(std::time(0));
+	std::vector<std::string>	nums;
+
+	for (int i = 0; i < 3000; i++){
+		nums.push_back(std::to_string(rand() % 1000 + 1));	
+	}
+	std::pair<std::vector<std::string>, int>	tests[] = {
+		std::make_pair(nums, 1000000)
+	};
+	runTests("RANDOM NUMBER TEST CASE", sizeof(tests) / sizeof(tests[0]), tests);
+	return;
+}
+
 int	main(void){
 	testValid();
+	testInvalid();
+	testRandom();
 	return 0;
 }
